631281b2f99b42d59ec9d3af282419ce96426a4f
Gunawan-aji <gunawanwijayanto533@gmail.com>
2024-06-28 15:50:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('khodamForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const birthdate = document.getElementById('birthdate').value;
+
+    const khodamResult = checkKhodam(name, birthdate);
+    document.getElementById('result').textContent = `Khodam anda adalah : ${khodamResult}`;
+});
+
+function checkKhodam(name, birthdate) {
+    const khodamList = [
+        'Kosong', 'Bedul', 'Oray Kadut', 'Kontolodon', 'Buaya Darat', 'siput'
+    ];
+
+    
+    const randomIndex = Math.floor(Math.random() * khodamList.length);
+    return khodamList[randomIndex];
+}
