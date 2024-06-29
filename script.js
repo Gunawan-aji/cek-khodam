58a19f459c018d36d5847ef2e16d66d56a006143
Gunawan-aji <gunawanwijayanto533@gmail.com>
2024-06-29 21:53:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.getElementById('khodamForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const birthdate = document.getElementById('birthdate').value;
+  const name = document.getElementById('name').value;
+  const birthdate = document.getElementById('birthdate').value;
 
-    const khodamResult = checkKhodam(name, birthdate);
-    document.getElementById('result').textContent = `Khodam anda adalah : ${khodamResult}`;
+  const khodamResult = checkKhodam(name, birthdate);
+  document.getElementById('result').textContent = `Khodam anda adalah : ${khodamResult}`;
 });
 
 function checkKhodam(name, birthdate) {
-    const khodamList = [
-        'Kosong', 'Bedul', 'Oray Kadut', 'Kontolodon', 'Buaya Darat', 'siput'
-    ];
+  const khodamList = [
+      'Kosong', 'Bedul', 'Oray Kadut', 'Kontolodon', 'Buaya Darat', 'siput', 'Tobrut', 'Kodomo'
+  ];
 
-    
-    const randomIndex = Math.floor(Math.random() * khodamList.length);
-    return khodamList[randomIndex];
+
+  const randomIndex = Math.floor(Math.random() * khodamList.length);
+  return khodamList[randomIndex];
 }
